30255eff6c53c6c4f6423e7bf710422aae1b3b40
Thomas Huysmans <thomas@Thomass-MacBook-Pro.local>
2017-08-13 08:11:26 +0200

    Changed to https

--- Scripts/contact_me.js
+++ Scripts/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://formspree.io/thomas@kaizenit.consulting",
+                url: "https://formspree.io/thomas@kaizenit.consulting",
                 type: "POST",
                 data: {
                     name: name,
